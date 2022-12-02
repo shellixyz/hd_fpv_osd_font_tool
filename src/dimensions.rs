2b4f581c1b89c3aa99bcd7420b454dd318fe673e
Michel Pastor <shellixyz@gmail.com>
2022-12-02 23:42:24 +0100

    add generic Dimensions parsing

--- src/dimensions.rs
+++ src/dimensions.rs
@@ -1,8 +1,11 @@
 
-use std::{ops::Mul, fmt::Display};
+use std::{ops::Mul, fmt::Display, str::FromStr};
 
 use derive_more::{From, Sub, Div};
 use getset::CopyGetters;
+use regex::Regex;
+use thiserror::Error;
+use lazy_static::lazy_static;
 
 
 #[derive(CopyGetters, PartialEq, Eq, PartialOrd, Ord, From, Debug, Clone, Copy, Div, Sub)]
@@ -45,5 +48,29 @@ impl<T: PartialOrd + Ord + Copy + Mul<Output = T> + Display> Mul for Dimensions<
 
     fn mul(self, rhs: Self) -> Self::Output {
         Self { width: self.width * rhs.width, height: self.height * rhs.height }
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("invalid dimensions format: {0}")]
+pub struct FormatError(String);
+
+impl<T> FromStr for Dimensions<T>
+where
+    T: PartialEq + Eq + PartialOrd + Ord + Copy + Clone + Display + FromStr,
+    <T as FromStr>::Err: std::fmt::Debug
+{
+    type Err = FormatError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! { static ref ORIGIN_RE: Regex = Regex::new(r"\A(?P<width>\d+)x(?P<height>\d+)\z").unwrap(); }
+        match ORIGIN_RE.captures(s) {
+            Some(captures) => {
+                let width: T = captures.name("width").unwrap().as_str().parse().map_err(|_| FormatError(s.to_owned()))?;
+                let height: T = captures.name("height").unwrap().as_str().parse().map_err(|_| FormatError(s.to_owned()))?;
+                Ok(Self { width, height })
+            },
+            None => Err(FormatError(s.to_owned())),
+        }
     }
 }
